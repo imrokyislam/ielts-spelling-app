212b81727de6badaea54a81f52f00b16bf8072cc
imrokyislam <04rokyislam@gmail.com>
2025-08-25 23:24:13 -0700

    Create defaultWords.js

---
+++ data/defaultWords.js
@@ -0,0 +1,20 @@
+/**
+ * A small set of default words for practice. The user can extend
+ * this list or provide their own custom list in the Custom Section.
+ */
+const defaultWords = [
+  'accommodate',
+  'appearance',
+  'conscience',
+  'hierarchy',
+  'maintenance',
+  'occurrence',
+  'pharaoh',
+  'supersede',
+  'weird',
+  'tomorrow',
+  'February',
+  'believe',
+]
+
+export default defaultWords
