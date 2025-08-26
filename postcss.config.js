cff08d56196b79c2bde0c9910582c65a7751d694
imrokyislam <04rokyislam@gmail.com>
2025-08-25 23:17:16 -0700

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+/**
+ * PostCSS configuration
+ *
+ * This file wires up Tailwind CSS and Autoprefixer so that
+ * your CSS is automatically transformed when building the site.
+ */
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
